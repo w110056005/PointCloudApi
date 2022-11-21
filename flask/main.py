e23e4689f58db19345b564c50ce300f2a4a929f1
Leon <w110056005@mail.nchu.edu.tw>
2022-11-21 21:53:56 +0800

    Update main.py

--- flask/main.py
+++ flask/main.py
@@ -9,6 +9,23 @@ from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import check_password_hash, generate_password_hash
 from pathlib import Path
 
+def abort_msg(e):
+    """500 bad request for exception
+
+    Returns:
+        500 and msg which caused problems
+    """
+    error_class = e.__class__.__name__  # 引發錯誤的 class
+    detail = e.args[0]  # 得到詳細的訊息
+    cl, exc, tb = sys.exc_info()  # 得到錯誤的完整資訊 Call Stack
+    lastCallStack = traceback.extract_tb(tb)[-1]  # 取得最後一行的錯誤訊息
+    fileName = lastCallStack[0]  # 錯誤的檔案位置名稱
+    lineNum = lastCallStack[1]  # 錯誤行數
+    funcName = lastCallStack[2]  # function 名稱
+    # generate the error message
+    errMsg = "Exception raise in file: {}, line {}, in {}: [{}] {}.".format(fileName, lineNum, funcName, error_class, detail)
+    # return 500 code
+    abort(500, errMsg)
 
 app = Flask(__name__)
 app.config['SWAGGER'] = {
@@ -35,26 +52,6 @@ swagger_template = {
 }
 
 
-def abort_msg(e):
-    """500 bad request for exception
-
-    Returns:
-        500 and msg which caused problems
-    """
-    error_class = e.__class__.__name__  # 引發錯誤的 class
-    detail = e.args[0]  # 得到詳細的訊息
-    cl, exc, tb = sys.exc_info()  # 得到錯誤的完整資訊 Call Stack
-    lastCallStack = traceback.extract_tb(tb)[-1]  # 取得最後一行的錯誤訊息
-    fileName = lastCallStack[0]  # 錯誤的檔案位置名稱
-    lineNum = lastCallStack[1]  # 錯誤行數
-    funcName = lastCallStack[2]  # function 名稱
-    # generate the error message
-    errMsg = "Exception raise in file: {}, line {}, in {}: [{}] {}. Please contact the member who is the person in charge of project!".format(
-        fileName, lineNum, funcName, error_class, detail)
-    # return 500 code
-    abort(500, errMsg)
-
-
 Swagger(app, template=swagger_template)
 
 DOWNLOAD_DIRECTORY = '/app/files/'
@@ -66,29 +63,11 @@ def verify_password(username, password):
         return check_password_hash(users.get(username), password)
     return False
 
-
-@app.route('/hello-world', methods=['GET'])
-@auth.login_required
-def hello_world():
-    """
-      Return Hello World 
-      ---
-      tags:
-        - Node APIs
-      produces: application/json,
-      responses:
-        200:
-          description: Return Hello World 
-          examples:
-            "Hello World"
-    """
-    return "Hello World"
-
-
 @app.route('/files/<id>', methods=['GET'])
 @auth.login_required
 def get_files():
     """
+
       Get registrated point cloud file
       ---
       tags:
@@ -108,6 +87,23 @@ def get_files():
     except Exception as e:
         abort_msg(e)
 
+@app.route('/file', methods=['GET'])
+@auth.login_required
+def get_file():
+    """
+      Get file
+      ---
+      tags:
+        - Node APIs
+      produces: application/json,
+      responses:
+        200:
+          description: Return pcd
+    """
+    try:
+        return send_from_directory('/app', 'test.txt', as_attachment=True)
+    except Exception as e:
+        abort_msg(e)
 
 @app.route('/registration', methods=['POST'])
 @auth.login_required
