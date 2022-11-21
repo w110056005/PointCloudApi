ca944f194f5bfd4e84f9f9a515ed293ed0f80122
Leon <w110056005@mail.nchu.edu.tw>
2022-11-21 21:29:07 +0800

    Update main.py and requirements.txt

--- flask/main.py
+++ flask/main.py
@@ -1,7 +1,9 @@
 import subprocess
 import datetime
 import os
-from flask import Flask, request, send_from_directory
+import traceback
+
+from flask import Flask, request, send_from_directory, abort
 from flasgger import Swagger
 from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -32,9 +34,31 @@ swagger_template = {
     },
 }
 
+
+def abort_msg(e):
+    """500 bad request for exception
+
+    Returns:
+        500 and msg which caused problems
+    """
+    error_class = e.__class__.__name__  # 引發錯誤的 class
+    detail = e.args[0]  # 得到詳細的訊息
+    cl, exc, tb = sys.exc_info()  # 得到錯誤的完整資訊 Call Stack
+    lastCallStack = traceback.extract_tb(tb)[-1]  # 取得最後一行的錯誤訊息
+    fileName = lastCallStack[0]  # 錯誤的檔案位置名稱
+    lineNum = lastCallStack[1]  # 錯誤行數
+    funcName = lastCallStack[2]  # function 名稱
+    # generate the error message
+    errMsg = "Exception raise in file: {}, line {}, in {}: [{}] {}. Please contact the member who is the person in charge of project!".format(
+        fileName, lineNum, funcName, error_class, detail)
+    # return 500 code
+    abort(500, errMsg)
+
+
 Swagger(app, template=swagger_template)
 
 DOWNLOAD_DIRECTORY = '/app/files/'
+
 
 @auth.verify_password
 def verify_password(username, password):
@@ -64,25 +88,25 @@ def hello_world():
 @app.route('/files/<id>', methods=['GET'])
 @auth.login_required
 def get_files():
-   """
-     Get registrated point cloud file
-     ---
-     tags:
-       - Node APIs
-     produces: application/json,
-     parameters:
-     - name: id
-       in: path
-       type: string
-       required: true
-     responses:
-       200:
-         description: Return pcd
-   """
-   try:
-       return send_from_directory(DOWNLOAD_DIRECTORY+'/'+id, id+'.pcd', as_attachment=True)
-   except FileNotFoundError:
-       abort(404)
+    """
+      Get registrated point cloud file
+      ---
+      tags:
+        - Node APIs
+      produces: application/json,
+      parameters:
+      - name: id
+        in: path
+        type: string
+        required: true
+      responses:
+        200:
+          description: Return pcd
+    """
+    try:
+        return send_from_directory(DOWNLOAD_DIRECTORY+'/'+id, id+'.pcd', as_attachment=True)
+    except Exception as e:
+        abort_msg(e)
 
 
 @app.route('/registration', methods=['POST'])
